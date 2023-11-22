6ffa8c65468032188708a96549f007b688d33c72
Nadeem Hussain <M.Nadeem.Hussain@outlook.com>
2023-11-22 10:31:37 +0000

    finished hompage

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -75,51 +76,262 @@
     <section>
       <div class="container-fluid">
         <div class="row">
-          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Mens.jpg" alt="Mens"><a class="labels" href="#">Mens</a></div>
-          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Womens.jpg" alt="Womens"><a class="labels" href="#">Womens</a></div>
-          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Kid.jpg" alt="kids"><a class="labels" href="#">Kids</a></div>
+          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Mens.jpg" alt="Mens"><a
+              class="labels" href="#">Mens</a></div>
+          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Womens.jpg" alt="Womens"><a
+              class="labels" href="#">Womens</a></div>
+          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Kid.jpg" alt="kids"><a
+              class="labels" href="#">Kids</a></div>
         </div>
       </div>
     </section>
 
     <!--Best sellers -->
 
-    <section>
+    <!-- <section>
       <div class="container my-5 best-sellers">
         <div class="row">
           <div class="col productbox">
-            <div class="product border"><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
+            <div class="product "><img class="img-fluid" src="..\TheZone\images\product4.webp" alt="prduct"></div>
           </div>
           <div class="col productbox">
-            <div class="product border"><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
+            <div class="product "><img class="img-fluid" src="..\TheZone\images\product5.webp" alt="prduct"></div>
           </div>
           <div class="col productbox">
-            <div class="product border"><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
+            <div class="product "><img class="img-fluid" src="..\TheZone\images\product6.webp" alt="prduct"></div>
           </div>
           <div class="col productbox">
-            <div class="product border"><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
+            <div class="product "><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
           </div>
         </div>
         
         <div class="row">
           <div class="col productbox">
-            <div class="product border"><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
+            <div class="product "><img class="img-fluid" src="..\TheZone\images\product1.webp" alt="prduct"></div>
           </div>
           <div class="col productbox">
-            <div class="product border"><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
+            <div class="product "><img class="img-fluid" src="..\TheZone\images\product2.webp" alt="prduct"></div>
           </div>
           <div class="col productbox">
-            <div class="product border"><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
+            <div class="product"><img class="img-fluid" src="..\TheZone\images\product3.webp" alt="prduct"></div>
           </div>
           <div class="col productbox">
-            <div class="product border"><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
+            <div class="product "><img class="img-fluid" src="..\TheZone\images\product.webp" alt="prduct"></div>
           </div>
 
         </div>
       </div>
+    </section> -->
+    <section class="best-selling">
+      <div class="container-fluid">
+        <header class="title">Best-selling</header>
+        <div class="row item-boxes">
+          <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product1.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product2.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product3.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product4.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
 
 
+        <div class="row item-boxes">
+          <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product1.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product2.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product3.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product4.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </section>
+
+    <!-- banner -->
+    <section>
+      <div class="container-fluid banner">
+        <img class="img-fluid"
+          src="..\TheZone\images\portrait-young-woman-dressed-hoodie-ripped-jeans-leaning-wall-while-sitting-skateboard-bridge-footway2.jpg"
+          alt="banner">
+        <div class="banner-txt">
+          <h2>Ethics meet style.</h2>
+          <h4>Effortless, elevated, easy-to-wear.</h4>
+          <a id="banner-btn" href="#">SHOP NOW</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="best-selling">
+      <div class="container-fluid">
+        <header class="title">COMMING SOON</header>
+        <div class="row item-boxes">
+          <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product1.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product2.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..\TheZone\images\product3.webp" alt="Card image cap">
+              <div class="card-body">
+                <p><strong>Product Title</strong></p>
+                <p class="card-text">£10.99</p>
+                <p><span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <!-- social media -->
+
+    <section>
+      <div class="social">
+      <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+<div class="elfsight-app-6bac2b4d-54be-4dcd-ba8f-253d2a9fd62f" data-elfsight-app-lazy></div>
+    </section>
+
+
 
 
   </main>
