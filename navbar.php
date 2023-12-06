e753b7bfed6f89741548f22e163d98e8c8278dad
HamzaAbdulla <148326802+HamzaAbdulla@users.noreply.github.com>
2023-12-06 12:01:39 +0000

    Added the contact us page link to the nav bar

--- navbar.php
+++ navbar.php
@@ -13,6 +13,9 @@
           <li class="nav-item">
             <a class="nav-link" href="aboutus.php">About Us</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contacts.php">Contact Us</a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Products
