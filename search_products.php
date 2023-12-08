b84294cef0c10aae5a8df84e6e0494c222406610
Nadeem Hussain <M.Nadeem.Hussain@outlook.com>
2023-12-08 19:06:39 +0000

    fixed bug with drop-down menu not showing on some pages

--- search_products.php
+++ search_products.php
@@ -54,7 +54,8 @@ session_start();
 
 
 
-
+<!-- needed for drop down menu -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 
 </html>
