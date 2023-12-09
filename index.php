1052197f2bd980ac5a8f6b6f4e0923b36fbc1e7a
Nadeem Hussain <M.Nadeem.Hussain@outlook.com>
2023-12-09 13:16:32 +0000

    added links to products pages in menus

--- index.php
+++ index.php
@@ -48,8 +48,8 @@ session_start();
     <section>
       <div class="container-fluid">
         <div class="row">
-          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Mens.jpg" alt="Mens"><a class="labels" href="#">Mens</a></div>
-          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\womens-menu.jpg" alt="Womens"><a class="labels" href="#">Womens</a></div>
+          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Mens.jpg" alt="Mens"><a class="labels" href="..\TheZone\products-men.php">Mens</a></div>
+          <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\womens-menu.jpg" alt="Womens"><a class="labels" href="..\TheZone\products-women.php">Womens</a></div>
           <div class="col menubox"><img class="img-fluid menu" src="..\TheZone\images\Kid.jpg" alt="kids"><a class="labels" href="#">Kids</a></div>
         </div>
       </div>
