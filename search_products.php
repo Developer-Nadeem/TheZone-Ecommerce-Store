082f950e6603c0573d2aa10aa432639b72dac937
khizzer123 <42179234+khizzer123@users.noreply.github.com>
2023-12-10 18:10:53 +0000

    Add add to cart

--- search_products.php
+++ search_products.php
@@ -1,5 +1,22 @@
 <?php
 session_start();
+
+if (!isset($_COOKIE['shopping_cart'])) {
+  setcookie('shopping_cart', serialize(array()), time() + (86400), "/"); //Shopping cart cookie expires in a day
+  setcookie('shopping_cart_json', json_encode(array()), time() + (86400), "/"); //Shopping cart cookie expires in a day
+}
+
+if (isset($_POST['add-to-cart'])) {
+  $productId = $_POST['product-id'];
+
+  $shoppingCart = isset($_COOKIE['shopping_cart']) ? unserialize($_COOKIE['shopping_cart']) : array();
+
+  array_push($shoppingCart, $productId);
+
+  setcookie('shopping_cart', serialize($shoppingCart), time() + (86400), "/"); //Shopping cart cookie expires in a day
+
+  setcookie('shopping_cart_json', json_encode($shoppingCart), time() + (86400), "/"); //Shopping cart cookie expires in a day
+}
 ?>
 
 <!DOCTYPE html>
