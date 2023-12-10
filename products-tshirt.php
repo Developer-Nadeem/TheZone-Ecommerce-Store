0c9053de9efc12b3ea54199b4e86f3964c394c27
HamzaAbdulla <148326802+HamzaAbdulla@users.noreply.github.com>
2023-12-10 21:10:31 +0000

    Added the footer to all the product pages

--- products-tshirt.php
+++ products-tshirt.php
@@ -72,6 +72,10 @@ if (isset($_POST['add-to-cart'])) {
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
   </script>
+
+  <!-- Footer Start -->
+  <?php include('..\TheZone\footer.php') ?>
+  <!-- Footer End -->
 </body>
 
 </html>
