15423530cdd896167eb790bfd51ae603f849f286
HamzaAbdulla <148326802+HamzaAbdulla@users.noreply.github.com>
2023-12-10 14:43:00 +0000

    Reverted back to fares's version from (Dec 10th 2023, 12:58PM GMT)

--- shopping-cart.php
+++ shopping-cart.php
@@ -14,17 +14,54 @@ session_start();
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
     <style>
+        .total-section {
+            border: 2.5px solid black;
+            border-radius: 5px;
+            margin-top: auto;
+            margin-bottom: auto;
+            background-color: rgb(230, 230, 230);
+            width: 30%;
+        }
+
+        .total-section h4 {
+            text-align: center;
+            margin-top: 5px;
+        }
+
+        .whole-cart {
+            display: flex;
+            width: 95%;
+        }
+        
+        .cart-items {
+            border: 2.5px solid black;
+            border-radius: 5px;
+            width: 60%;
+            margin-top: 20px;
+            margin-bottom: 30px;
+            margin-right: auto;
+            margin-left: auto;
+            background-color: rgb(230, 230, 230);
+        }
+
+        .cart-items h3 {
+            padding-top: 5px;
+            padding-bottom: 5px;
+        }
+
         .sample-product {
-            border: 2px solid #000; 
-            border-radius: 10px;
+            background-color: white;
+            border: 2.5px solid #000; 
+            border-radius: 5px;
             padding: 10px; 
             margin-top: 10px; 
             text-align: left; 
-            width: 100%;
+            width: 50%;
             margin-left: auto;
             margin-right: auto; /* margin left and right set to auto to centre align the product container */
             overflow: hidden;
             position: relative;
+            display: flex;
         }
 
         .sample-product h3 {
@@ -35,15 +72,15 @@ session_start();
         .sample-product img {
             width: 120px; /* setting a fixed width for the image for consistency */
             height: auto; 
-            border: 3px solid #000; 
-            border-radius: 10px;
+            border: 2.5px solid #000; 
+            border-radius: 5px;
             margin: 15px;
 
         }
 
         .sample-product button {
             background-color: #ff0000; 
-            border-radius: 5px;
+            border-radius: 3px;
             color: #fff; 
             border: none; 
             margin: 15px;
@@ -58,8 +95,16 @@ session_start();
         .sample-product button:hover {
             background-color: #fff; /* changing background color on hover */
             color: #ff0000;
-            border: 2px solid #ff0000;
+            border: 2.5px solid #ff0000;
             padding: 5px 10px; 
+        }
+
+        hr {
+            height: 4px; /* Set the height to make it thicker */
+            width: 55%; /* Set the width to make it shorter (adjust as needed) */
+            margin: 20px auto; /* Center the line by setting margin and using auto for left and right */
+            background-color: #000; /* Set the background color of the line */
+            border: none; /* Remove the default border */
         }
     </style>
 
@@ -72,33 +117,72 @@ session_start();
     <!--Navbar End-->
 
     <main>
-        <div class="shoppingcart-container">
+        <!-- <div class="shoppingcart-container"> -->
+        <!-- <div class="shoppingcart-container">
             <h2 class="text-center">Your Cart</h2>
-            <ul id="cart-items" style="padding-left: 10px; padding-right: 10px">
-                <!-- Cart items will be dynamically added here@khizzer -->
+            <ul id="cart-items">
                 <?php
-                    $shopping_cart = isset($_COOKIE['shopping_cart']) ? $_COOKIE['shopping_cart'] : array();
-                    foreach(unserialize($shopping_cart) as $item) {
-                        echo '<div class="sample-product">';
-                        echo'<h3>'. $item .'</h3>';
-                        echo '<h3>£10.99</h3>';
-                        echo '<img src="..\TheZone\images\\' . $item. '.webp" alt="Sample Product Image">';
-                        echo '<button onclick="removeProduct()">Remove</button>';
-                        echo '</div>';
-                    }
+                    // $shopping_cart = $_COOKIE['shopping_cart'] ? $_COOKIE['shopping_cart'] : array();
+                    // foreach(unserialize($shopping_cart) as $item) {
+                    //     echo '<div class="cart-item" style="background-color: white; "';
+                    //     echo '<p> '. $item . '</p>';
+                    //     echo '</div>';
+                    // }
+                    // $shopping_cart = $_COOKIE['shopping_cart'] ? $_COOKIE['shopping_cart'] : array();
+                    // foreach(unserialize($shopping_cart) as $item) {
+                    //     echo '<div class="cart-item" style="background-color: white; "';
+                    //     echo '<p> '. $item . '</p>';
+                    //     echo '</div>';
+                    // }
                 ?>
             </ul>
             <p class="text-center">Total: £<span id="cart-total">0.00</span></p>
-            <!-- change the above to display total price of products -->
             <div style="text-align: center;">
                 <button class="shoppingcart-button" type="submit" name="checkout-button" onclick="checkout()">Check Out</button>
             </div>
-
         </div>
+        </div> -->
+
+        <!-- div section for the entire cart and the total amount display -->
+        <div class="whole-cart">
+
+            <!-- div section for cart items to be shown -->
+            <div class="cart-items">
+                <div style="background-color: #333; color: white; text-align: center;padding: 15px;">
+                    <h3>Your Cart</h3>
+                </div>
+                <div class="sample-product">
+                    <img src="..\TheZone\images\product2.webp" alt="Sample Product Image">
+                    <h3>Grey Zipped Sweater</h3>
+                    <h3>£10.99</h3>
+                    <button onclick="removeProduct()">Remove</button>
+                </div>
+                <hr style="">
+                <div class="sample-product">
+                    <img src="..\TheZone\images\product2.webp" alt="Sample Product Image">
+                    <h3>Grey Zipped Sweater</h3>
+                    <h3>£10.99</h3>
+                    <button onclick="removeProduct()">Remove</button>
+                </div>
+            </div>
+
+            <!-- div section to show the total and the option to checkout via button -->
+            <div class="total-section">
+                <h4>TOTAL</h4>
+                <hr>
+                <p class="text-center">Total Price: £<span id="cart-total">0.00</span></p>
+                <div style="text-align: center; margin-bottom: 15px;">
+                <button class="shoppingcart-button" type="submit" name="checkout-button" onclick="checkout()">Check Out</button>
+                </div>
+            </div>    
 
         <!-- div section for a product with its name, price and product image -->
 
     </main>
+
+    <!-- Footer Start -->
+    <?php include('..\TheZone\footer.php') ?>
+    <!-- Footer End -->
 
 
     <script>
