51c22ccbe9f401b704ecf70bed6ad6b3d27d31f5
ibrahim91234 <220118581@aston.ac.uk>
2024-02-02 14:53:44 +0000

    contact us page

--- contacts.php
+++ contacts.php
@@ -7,10 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>TheZone - Products</title>
   <link rel="stylesheet" href="style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 
 <body>
@@ -20,23 +18,28 @@
 
   <h2 class="text-center">Contact Us</h2>
   <div class="container container-pos">
-  <h2>Contact Us</h2>
+    <div class="contact-container">
+      <form method="post" action="contacts.php" class="contact-form">
+        <label for="name" class="contact-label">Name:</label>
+        <input type="text" name="name" class="contact-input cfield" required><br>
 
-    <form method="post" action="contacts.php">
-      <label class="name-label" for="name">Name:</label>
-      <input class="name inputs" type="text" id="name" name="name" required><br>
+        <label for="email" class="contact-label">Email:</label>
+        <input type="email" name="email" class="contact-input cfield" required><br>
 
-      <label class="email-label" for="email">Email:</label>
-      <input class="email inputs" type="email" id="email" name="email" required><br>
-
-      <label class="message-label" for="message">Message:</label>
-      <textarea class="message inputs" id="message" name="message" rows="4" required></textarea><br>
-      <input class="submit-btn" type="submit" value="Submit">
-      <input type="hidden" name="submitted" value="true">
-    </form>
+        <label for="message" class="contact-label">Message:</label>
+        <textarea name="message" rows="4" class="contact-input cfield" required></textarea><br>
+        <input class="contact-input-submit" type="submit" value="Submit">
+        <input type="hidden" name="submitted" value="true">
+      </form>
+    </div>
   </div>
- <!-- needed for drop down menu -->
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+
+  <!-- needed for drop down menu -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+
+  <?php include("footer.php"); ?>
 </body>
 
 <style>
@@ -100,47 +103,7 @@ if (isset($_POST["submitted"]) && !empty(isset($_POST["message"])) && !empty(iss
     $query->bindParam(':message', $message);
     $query->bindParam(':Timestamp', $currentDateTime);
     $query->execute();
-
-
-
-  }catch(PDOException $ex){
-
-
-
-
+  } catch (PDOException $ex) {
+  }
 }
-
-
-}
-
-
-
-
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
