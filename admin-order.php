ce3d237b1d432f46db1019c4aa4d4ea20a463354
zain3695 <zainhussain3695@gmail.com>
2024-02-07 12:51:29 +0000

    created pages

---
+++ admin-order.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TheZone</title>
+    <link rel="stylesheet" href="../TheZone/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Order page</title>
+</head>
+
+<body>
+    <?php include('../TheZone/adminnavbar.php') ?>
+
+    <h1>
+
+        text
+
+    </h1>
+</body>
+
+</html>
--- adminnavbar.php
+++ adminnavbar.php
@@ -1,36 +1,40 @@
 <header>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.php"><img class="img-fluid logo" src="../TheZone/images/logo-tp.png" alt="Logo"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent" style="display: flex; justify-content: space-between; align-items: center; width:100%;">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="index.php">Inventory</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="aboutus.php">Customers</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contacts.php">Orders</a>
-          </li>
-        
-        </ul>
-        
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <?php
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img class="img-fluid logo" src="../TheZone/images/logo-tp.png"
+                    alt="Logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent"
+                style="display: flex; justify-content: space-between; align-items: center; width:100%;">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin-customer.php">Inventory</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin-inventory.php">Customers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin-order.php">Orders</a>
+                    </li>
+
+                </ul>
+
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <?php
             if (isset($_SESSION['email'])) {
               echo '<li><a href="logout.php" class="btn btn-outline-dark navbar-btn">Log Out<img src="images/logout-icon.png" alt="Logout" width="20" height="20"></a></li>';
             } else {
               echo '<li><a href="login-signup-page.php"class="btn btn-outline-dark">Login<img src="images/login-icon.png" alt="Login/Signup" width="20" height="20"></a></li>';
             }
             ?>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
