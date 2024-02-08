2a82e0555b1bcb14601d76416eb9adddb6572ec9
zain3695 <zainhussain3695@gmail.com>
2024-02-08 00:06:16 +0000

    minor changes to inventory page with dummy data

--- admin-inventory.php
+++ admin-inventory.php
@@ -14,12 +14,203 @@
 
 <body>
     <?php include('../TheZone/adminnavbar.php') ?>
-    <h1>
+    <main class="container">
+        <div id="chart-container">
+            <canvas id="myChart" width="400" height=" 200"></canvas>
+        </div>
 
-        text
+        <div id="container">
 
-    </h1>
-</body>
+            <div class="product-list">
+
+            </div>
+        </div>
+        <style>
+        .container {
+
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+
+        #chart-container {
+            flex: 1;
+            margin-right: 20px;
+
+        }
+
+        .product-list {
+
+            flex: 1;
+        }
+
+        .product-list {
+
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+            gap: 50px;
+
+        }
+
+        .product {
+            border: 5px solid #ccc;
+            padding: 20px;
+            text-align: center;
+
+        }
+
+        .product img {
+
+            max-width: 100%;
+            height: auto;
+        }
+        </style>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script>
+        const dummyProducts = [{
+                name: 'Product 1',
+                imageUrl: 'images/beautiful-smiling-model-with-horns-hairstyle-dressed-summer-hipster-jacket-jeans-clothes-sexy-carefree-girl-posing-street-trendy-funny-positive-woman-having-fun-sunglasses.jpg',
+                price: 20,
+                quantity: 50,
+                sales: {
+                    today: 50,
+                    thisMonth: 90,
+                    thisYear: 200
+                }
+            },
+            {
+                name: 'Product 2',
+                imageUrl: 'images/beautiful-smiling-model-with-horns-hairstyle-dressed-summer-hipster-jacket-jeans-clothes-sexy-carefree-girl-posing-street-trendy-funny-positive-woman-having-fun-sunglasses.jpg',
+                price: 30,
+                quantity: 60,
+                sales: {
+                    today: 50,
+                    thisMonth: 90,
+                    thisYear: 250
+                }
+            },
+            {
+                name: 'Product 3',
+                imageUrl: 'images/young-couple-wearing-trucker-hat.jpg',
+                price: 140,
+                quantity: 40,
+                sales: {
+                    today: 70,
+                    thisMonth: 90,
+                    thisYear: 500
+                }
+            },
+            {
+                name: 'Product 4',
+                imageUrl: 'images/fashion-model-with-beautiful-face-perfect-body-wearing-trendy-dress-holding-brown-leather-hand-bag-full-length.jpg',
+                price: 58,
+                quantity: 90,
+                sales: {
+                    today: 60,
+                    thisMonth: 90,
+                    thisYear: 200
+                }
+            },
+            {
+                name: 'Product 5 ',
+                imageUrl: 'images/fashion-model-with-beautiful-face-perfect-body-wearing-trendy-dress-holding-brown-leather-hand-bag-full-length.jpg',
+                price: 170,
+                quantity: 30,
+                sales: {
+                    today: 70,
+                    thisMonth: 95,
+                    thisYear: 300
+                }
+            }
+
+        ];
+
+
+
+        function displayProductList(products) {
+            const productListDiv = document.querySelector('.product-list');
+
+            products.forEach(product => {
+                const productDiv = document.createElement('div');
+                productDiv.classList.add('product');
+
+                const image = document.createElement('img');
+                image.src = product.imageUrl;
+                image.alt = product.name;
+                productDiv.appendChild(image);
+
+                const name = document.createElement('p');
+                name.textContent = `Name: ${product.name}`;
+                productDiv.appendChild(name);
+
+                const price = document.createElement('p');
+                price.textContent = `Price: £${product.price}`;
+                productDiv.appendChild(price);
+
+                const stockQuantity = document.createElement('p');
+                stockQuantity.textContent = `Stock Quantity: ${product.quantity}`;
+                productDiv.appendChild(stockQuantity);
+
+                const todaySales = document.createElement('p');
+                todaySales.textContent = `Today's sales: ${product.sales.today}`;
+                productDiv.appendChild(todaySales);
+
+                const thisMonthSales = document.createElement('p');
+                thisMonthSales.textContent = `This Month's sales: ${product.sales.thisMonth}`;
+                productDiv.appendChild(thisMonthSales);
+
+                const thisYearSales = document.createElement('p');
+                thisYearSales.textContent = `This Year's sales: ${product.sales.thisYear}`;
+                productDiv.appendChild(thisYearSales);
+
+                productListDiv.appendChild(productDiv);
+            });
+        }
+
+        displayProductList(dummyProducts);
+
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: dummyProducts.map(product => product.name),
+                datasets: [{
+                        label: 'Today\'s Sales',
+                        data: dummyProducts.map(product => product.sales.today),
+                        backgroundColor: 'rgba(9, 122, 62, 0.8)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 5
+                    },
+                    {
+                        label: 'This Month\'s Sales',
+                        data: dummyProducts.map(product => product.sales.thisMonth),
+                        backgroundColor: 'rgba(249, 48, 188, 0.8)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 5
+                    },
+                    {
+                        label: 'This Year\'s Sales',
+                        data: dummyProducts.map(product => product.sales.thisYear),
+                        backgroundColor: 'rgba(156, 166, 223, 0.8)',
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 5
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        </script>
+
+    </main>
+
 </body>
 
 </html>
--- adminnavbar.php
+++ adminnavbar.php
@@ -12,6 +12,9 @@
                 style="display: flex; justify-content: space-between; align-items: center; width:100%;">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
+                        <a class="nav-link" href="adminhomepage.php">Overview</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="admin-inventory.php">Inventory</a>
                     </li>
                     <li class="nav-item">
@@ -20,6 +23,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="admin-order.php">Orders</a>
                     </li>
+
 
                 </ul>
 
