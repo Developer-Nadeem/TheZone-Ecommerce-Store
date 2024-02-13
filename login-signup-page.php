a73baf352796147e77756729c6f3c2de01456270
HamzaAbdulla <148326802+HamzaAbdulla@users.noreply.github.com>
2024-02-13 16:49:05 +0000

    Fixed bug where you can still go to the login page even if the user is logged in

--- login-signup-page.php
+++ login-signup-page.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+
+// Checks if the user is logged in, if not it redirects you to the login/signup page
+if (isset($_SESSION['email'])) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
