c66e40b88d9c6efe4636782fc816edc355d3c7b2
khizzer123 <42179234+khizzer123@users.noreply.github.com>
2024-03-24 15:29:45 +0000

    Order depletes inventory and works with new shopping cart format

--- order.php
+++ order.php
@@ -22,7 +22,10 @@
         $user_id = $row['UserID'];
 
 
-        foreach($shopping_cart as $product_id) {
+        foreach($shopping_cart as $item => $itemArray) {
+            list($product_id, $size) = explode('|', $item);
+            $quantity = $itemArray['quantity'];
+
             $stmt = $db->query("SELECT Price FROM inventory WHERE ProductID = $product_id");
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -51,16 +54,37 @@
 
         $order_id = $db->lastInsertId();
 
-        foreach ($shopping_cart as $product_id) {
-            $stmt = $db->query("SELECT ProductName, Price FROM inventory WHERE ProductID = $product_id");
+        foreach($shopping_cart as $item => $itemArray) {
+            list($product_id, $size) = explode('|', $item);
+            $quantity = $itemArray['quantity'];
+
+            $stmt = $db->query("SELECT Price FROM inventory WHERE ProductID = $product_id");
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
-            $stmt = $db->prepare("INSERT INTO orderitems (OrderID, ProductID, Quantity) VALUES (:orderid ,:productid, :quantity)");
-            $stmt->bindValue(':orderid', $order_id, PDO::PARAM_INT);
-            $stmt->bindValue(':productid', $product_id, PDO::PARAM_INT);
-            $stmt->bindValue(':quantity', 1, PDO::PARAM_INT);
+            $price = $row['Price'];
+            
+            $stmt = $db->prepare("INSERT INTO orderitems (OrderID, ProductID, Quantity, SubTotal) VALUES (:order_id ,:product_id, :quantity, :sub_total)");
+            $stmt->bindValue(':order_id', $order_id, PDO::PARAM_INT);
+            $stmt->bindValue(':product_id', $product_id, PDO::PARAM_INT);
+            $stmt->bindValue(':quantity', $quantity, PDO::PARAM_INT);
+            $stmt->bindValue(':sub_total', $quantity * $price, PDO::PARAM_INT);
             $stmt->execute();
+
+            $stmt = $db->prepare("
+            SELECT stock_table.* 
+            FROM stock_table 
+            INNER JOIN sizes_table ON stock_table.SizeID = sizes_table.SizeID 
+            WHERE stock_table.ProductID = :product_id AND sizes_table.SizeName = :size
+            ");
+
+            $stmt->execute([':product_id' => $product_id, ':size' => $size]);
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            
+            foreach ($rows as $row) {
+                $newStock = $row['Quantity'] - $quantity;
+
+                $stmt = $db->prepare("UPDATE stock_table SET Quantity = :stock WHERE StockID = :stock_id");
+                $stmt->execute([':stock' => $newStock, ':stock_id' => $row['StockID']]);
+            }
         };
 
         setcookie('shopping_cart', serialize(array()), time() + (86400), "/"); //Shopping cart cookie expires in a day
