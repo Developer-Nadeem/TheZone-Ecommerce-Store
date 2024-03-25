a1651f276425761f4dbb0d392106880299924804
HamzaAbdulla <148326802+HamzaAbdulla@users.noreply.github.com>
2024-03-25 11:06:40 +0000

    Fixed bug where you could buy a product without selecting the size

--- product-page.php
+++ product-page.php
@@ -22,21 +22,21 @@ if (isset($_POST['add-to-cart'])) {
 
   if (!isset($size) || empty($size)) {
     echo '<script>alert("Please select a size")</script>';
-  };
-
-  $shopping_cart = isset($_COOKIE['shopping_cart']) ? unserialize($_COOKIE['shopping_cart']) : array();
-  $quantity = isset($_POST['quantity']) ? $_POST['quantity'] : 1;
-
-  $productKey = $productId . '|' . $size;
-
-  if (array_key_exists($productKey, $shopping_cart)) {
-    $shopping_cart[$productKey]['quantity'] += $quantity;
   } else {
-    $shopping_cart[$productKey] = array('quantity' => $quantity, 'size' => $size);
-  }
+    $shopping_cart = isset($_COOKIE['shopping_cart']) ? unserialize($_COOKIE['shopping_cart']) : array();
+    $quantity = isset($_POST['quantity']) ? $_POST['quantity'] : 1;
 
-  setcookie('shopping_cart', serialize($shopping_cart), time() + (86400), "/"); //Shopping cart cookie expires in a day
-  setcookie('shopping_cart_json', json_encode($shopping_cart), time() + (86400), "/"); //Shopping cart cookie expires in a day
+    $productKey = $productId . '|' . $size;
+
+    if (array_key_exists($productKey, $shopping_cart)) {
+      $shopping_cart[$productKey]['quantity'] += $quantity;
+    } else {
+      $shopping_cart[$productKey] = array('quantity' => $quantity, 'size' => $size);
+    }
+
+    setcookie('shopping_cart', serialize($shopping_cart), time() + (86400), "/"); //Shopping cart cookie expires in a day
+    setcookie('shopping_cart_json', json_encode($shopping_cart), time() + (86400), "/"); //Shopping cart cookie expires in a day
+  }
 }
 
 // gets the rating of the product
@@ -138,7 +138,7 @@ if (isset($_SESSION['reviewSubmit'])) {
                   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
                   foreach ($rows as $row) {
                     $outOfStock = ($row['Quantity'] <= 0) ? 'disabled' : '';
-                    $noStockTxt = ($row['Quantity'] <=0) ? '(Out of Stock)' : '';
+                    $noStockTxt = ($row['Quantity'] <= 0) ? '(Out of Stock)' : '';
                     echo '<a class="dropdown-item ' . $outOfStock . '" name="sizeDropdownOption">' . $row['SizeName'] . $noStockTxt . '</a>';
                   } ?>
                 </div>
